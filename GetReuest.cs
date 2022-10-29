0a8b4c58deee7f3e4a3a5ba083c3e6b0bce16dd7
Kraiben <98973548+Fuflick@users.noreply.github.com>
2022-10-30 00:04:03 +0300

    Add files via upload

---
+++ GetReuest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hakaton_bot
+{
+    public class GetReuest
+    {
+        HttpWebRequest _request;
+        string _link;
+        public string Response { get; set; }
+        public GetReuest(string link)
+        {
+            this._link = link;
+        }
+        public void Run()
+        {
+            try
+            {
+                _request = (HttpWebRequest)WebRequest.Create(_link);
+                _request.Method = "GET";
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var sream = response.GetResponseStream();
+                if (sream != null) Response = new StreamReader(sream).ReadToEnd();
+            }
+            catch (Exception)
+            { 
+                throw;
+            }
+            
+        }
+    }
+}
